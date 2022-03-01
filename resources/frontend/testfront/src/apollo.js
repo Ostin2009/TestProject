5f935719ac8d0431ba108d8b03b7f6d3fe064944
Maxim <ostin2605@gmail.com>
2022-03-01 18:37:28 +0300

    Реализовано получение данных с помощью apollo apollo.js, usersList.vue, web.php

--- resources/frontend/testfront/src/apollo.js
+++ resources/frontend/testfront/src/apollo.js
@@ -1,11 +1,18 @@
 import { BatchHttpLink } from 'apollo-link-batch-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
-import { createUploadLink } from 'apollo-upload-client'
 
-export const apolloClient = new ApolloClient({
+const httpLink = new BatchHttpLink({
     uri: 'http://testproject.loc/graphql',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export const apollo = new ApolloClient({
     cache: new InMemoryCache(),
+    link: httpLink,
     shouldBatch: true,
     connectToDevTools: false
 })
--- resources/frontend/testfront/src/main.js
+++ resources/frontend/testfront/src/main.js
@@ -2,38 +2,14 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
-//import { createProvider } from './vue-apollo'
-//import { createHttpLink } from 'apollo-link-http'
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import gql from 'graphql-tag'
-
-const defaultClient = new ApolloClient({
-    uri: 'http://testproject.loc/graphql',
-    cache: new InMemoryCache()
-})
-
-const query = gql`
-    query {
-         users(size: 5, page: 3) {
-    items {
-      id
-      name
-      email
-    }
-    page
-    size
-    count
-    pages
-  }
-}`
-
-defaultClient.query({
-    query
-}).then(res => console.log(res))
+import router from "@/router/router";
+//import { DefaultApolloClient } from '@vue/apollo-composable'
+//import {apolloClient} from "@/apollo";
 
 const app = createApp(App)
 
+app.use(router)
 app.use(ElementPlus)
+//app.provide(DefaultApolloClient, apolloClient)
 app.mount('#app')
 
